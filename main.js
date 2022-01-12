ea6bc248a0773dffadcd67f6694c95e5d3d44139
FloCor <florian.corridoni@gmail.com>
2022-01-12 11:52:12 +0100

    modif yoooo

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+//yooooo 
+
+
 // NE PAS MODIFIER : Déclaration de variables utiles : varaibles qui peuvent aider à la résolution des exercices, mais qui ne sont pas forcément indispensables.
 alphabet=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 morse=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
